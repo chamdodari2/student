ca355b83ebd75d1de5b09dd6da4d9ff6388f80f5
yjkim <chitose_@naver.com>
2021-04-25 14:54:09 +0900

    사진추가 하는중

--- src/main/java/student/ui/panel/AddStudentPicPanel.java
+++ src/main/java/student/ui/panel/AddStudentPicPanel.java
@@ -1,23 +1,41 @@
 package student.ui.panel;
 
-import student.dto.StudentData;
-import student.ui.content.AbstractContentPanel;
 import java.awt.BorderLayout;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.Dimension;
-import javax.swing.JButton;
 import java.awt.Color;
-import javax.swing.SwingConstants;
-import javax.swing.ImageIcon;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.nio.file.Files;
 
-public class AddStudentPicPanel extends AbstractContentPanel<StudentData> {
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-	
+import student.dto.StudentData;
+import student.ui.content.AbstractContentPanel;
+
+@SuppressWarnings("serial")
+public class AddStudentPicPanel extends AbstractContentPanel<StudentData> implements ActionListener {
+	private JButton btnAddPic;
+	private String imgPath = System.getProperty("user.dir")+ File.separator+"images" +File.separator;
+	private File chooserFile  =	new File(imgPath);
+	private JLabel lblPic;
+	private JPanel panel;
+	// 
+
+	public File getChooserFile() {
+		return chooserFile;
+	}
 	public AddStudentPicPanel() {
-
+			System.out.println(System.getProperty("user.dir"));
 		initialize();
 	}
 	private void initialize() {
@@ -29,21 +47,31 @@ public class AddStudentPicPanel extends AbstractContentPanel<StudentData> {
 		flowLayout.setHgap(20);
 		add(pWest, BorderLayout.CENTER);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		pWest.add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblPic = new JLabel();
-		lblPic.setIcon(new ImageIcon("C:\\Users\\lenovo\\Desktop\\배수지.jpg"));
+		lblPic = new JLabel();
+	//	lblPic.setIcon(new ImageIcon("C:\\Users\\lenovo\\Desktop\\배수지.jpg"));
+	//	lblPic.setIcon(new ImageIcon(imgPath)); //선택 했던걸로 저장
+//		chooserFile = new File(chooserFile.getPath());
+		
+		
+		
 		lblPic.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPic.setText("사진넣을예정");
 		lblPic.setBackground(Color.DARK_GRAY);
 		lblPic.setPreferredSize(new Dimension(100, 150));
 		panel.add(lblPic, BorderLayout.CENTER);
 		
-		JButton btnNewButton = new JButton("사진 추가");
-		btnNewButton.setFont(new Font("굴림", Font.BOLD, 15));
-		panel.add(btnNewButton, BorderLayout.SOUTH);
+		btnAddPic = new JButton("사진 추가");
+		btnAddPic.addActionListener(this);
+		btnAddPic.setFont(new Font("굴림", Font.BOLD, 15));
+		panel.add(btnAddPic, BorderLayout.SOUTH);
+		
+		
+
+		
 	}
 
 	
@@ -73,4 +101,36 @@ public class AddStudentPicPanel extends AbstractContentPanel<StudentData> {
 		
 	}
 
-}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnAddPic) {
+			actionPerformedBtnAddPic(e);
+		
+		}
+	}
+	protected void actionPerformedBtnAddPic(ActionEvent e) {
+		String openPath = "C:\\workspace_java\\student\\document\\images";  //일로 들어가는게 기본화면
+		JFileChooser chooser = new JFileChooser(openPath); //주소를 여기 넣어준당
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & GIF & PNG image","jpg","gif","png");
+		chooser.setFileFilter(filter);
+		int res =chooser.showOpenDialog(null);
+		if (res != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(null, "파일을 선택하지 않았습니다.", "경고", JOptionPane.WARNING_MESSAGE);
+			return;
+		} else {
+
+			chooserFile = chooser.getSelectedFile();
+			System.out.println("chooserFile 1 >>> "+ chooserFile);
+//			System.out.println("chooserFile >>>" + chooserFile);  //김태희가 있는 주소를 가리킨다.
+			lblPic.setIcon(new ImageIcon(chooserFile.getPath()));			
+			System.out.println("chooserFile>>>2" +chooserFile.getPath());
+		
+			System.out.println();
+//			System.out.println(new ImageIcon(chooserFile.getPath()));
+//			lblPic.setIcon(new ImageIcon("C:\\Users\\lenovo\\Desktop\\배수지.jpg"));
+		
+
+		}
+	}
+		
+	}
+
